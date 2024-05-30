e71500c00ffdf9f6b1c6c0a8bae978c5175ab7fb
nicolethoen <nthoen@redhat.com>
2024-05-30 11:38:40 -0400

    fix: update to alpha omega release

--- packages/module/patternfly-docs/generated/extensions/log-viewer/design-guidelines.js
+++ packages/module/patternfly-docs/generated/extensions/log-viewer/design-guidelines.js
@@ -26,7 +26,7 @@ pageData.examples = {
 
 const Component = () => (
   <React.Fragment>
-    <p {...{"className":"ws-p"}}>
+    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
       {`A `}
       <strong>
         {`log viewer`}
@@ -36,52 +36,52 @@ const Component = () => (
     <AutoLinkHeader {...{"id":"light-theme-log-viewer","size":"h2","className":"ws-title ws-h2"}}>
       {`Light theme log viewer`}
     </AutoLinkHeader>
-    <img src={srcImport0} width={srcImport0.width} height={srcImport0.height} {...{"alt":"Log Viewer","className":"ws-img"}}>
+    <img src={srcImport0} width={srcImport0.width} height={srcImport0.height} {...{"alt":"Log Viewer","className":"ws-img "}}>
     </img>
-    <ol {...{"className":"ws-ol"}}>
-      <li {...{"className":"ws-li"}}>
+    <ol {...{"className":"undefinedpf-v6-c-content--ol ws-ol "}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         <strong>
           {`Type of log dropdown menu:`}
         </strong>
         {` Allow users to switch between different types of logs.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         <strong>
           {`Search bar:`}
         </strong>
         {` Use to look up historical logs. The results will be highlighted in the log.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         <strong>
           {`Pause button:`}
         </strong>
         {` Play and stop your log content viewing, instead of scrolling through.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         <strong>
           {`Clear log:`}
         </strong>
         {` Clear the displayed log output.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         <strong>
           {`Cog:`}
         </strong>
         {` House settings such as wrapping lines, showing timestamps, and displaying line numbers.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         <strong>
           {`Export:`}
         </strong>
         {` Export log content.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         <strong>
           {`Download:`}
         </strong>
         {` Download the log file.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         <strong>
           {`Fullscreen:`}
         </strong>
@@ -91,29 +91,29 @@ const Component = () => (
     <AutoLinkHeader {...{"id":"dark-theme-log-viewer","size":"h2","className":"ws-title ws-h2"}}>
       {`Dark theme log viewer`}
     </AutoLinkHeader>
-    <p {...{"className":"ws-p"}}>
+    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
       {`We recommend using the light theme editor by default, but there is also a dark theme log viewer
 available. All log viewer functionality remains the same whether a light or dark theme is used.`}
     </p>
-    <img src={srcImport1} width={srcImport1.width} height={srcImport1.height} {...{"alt":"Dark theme log viewer","className":"ws-img"}}>
+    <img src={srcImport1} width={srcImport1.width} height={srcImport1.height} {...{"alt":"Dark theme log viewer","className":"ws-img "}}>
     </img>
     <AutoLinkHeader {...{"id":"usability","size":"h2","className":"ws-title ws-h2"}}>
       {`Usability`}
     </AutoLinkHeader>
-    <p {...{"className":"ws-p"}}>
+    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
       {`Use a log viewer when:`}
     </p>
-    <ul {...{"className":"ws-ul"}}>
-      <li {...{"className":"ws-li"}}>
+    <ul {...{"className":"undefinedpf-v6-c-content--ul ws-ul "}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         {`The user can manipulate 1 large log file or multiple log files at the same time.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         {`You want to create a more readable and accessible environment for the user.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         {`The user may actively search for specific logs within a large log file.`}
       </li>
-      <li {...{"className":"ws-li"}}>
+      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
         {`The user shares files frequently with other users or teams.`}
       </li>
     </ul>
@@ -123,26 +123,26 @@ available. All log viewer functionality remains the same whether a light or dark
     <AutoLinkHeader {...{"id":"with-popover","size":"h3","className":"ws-title ws-h3"}}>
       {`With popover`}
     </AutoLinkHeader>
-    <p {...{"className":"ws-p"}}>
+    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
       {`The clear log button opens up a popover with further options, to prevent a user from accidentally clearing their log content.`}
     </p>
-    <img src={srcImport2} width={srcImport2.width} height={srcImport2.height} {...{"alt":"Clear this log popover open on a Log viewer","className":"ws-img"}}>
+    <img src={srcImport2} width={srcImport2.width} height={srcImport2.height} {...{"alt":"Clear this log popover open on a Log viewer","className":"ws-img "}}>
     </img>
     <AutoLinkHeader {...{"id":"with-dropdown-drilldown-and-search-expanded","size":"h3","className":"ws-title ws-h3"}}>
       {`With dropdown, drilldown, and search expanded`}
     </AutoLinkHeader>
-    <p {...{"className":"ws-p"}}>
+    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
       {`The cog icon in the log viewer toolbar allows you to store content options such as timestamps or line numbers for better orientation within the log viewer.`}
     </p>
-    <img src={srcImport3} width={srcImport3.width} height={srcImport3.height} {...{"alt":"Cog options open on a Log viewer","className":"ws-img"}}>
+    <img src={srcImport3} width={srcImport3.width} height={srcImport3.height} {...{"alt":"Cog options open on a Log viewer","className":"ws-img "}}>
     </img>
     <AutoLinkHeader {...{"id":"with-search-results","size":"h3","className":"ws-title ws-h3"}}>
       {`With search results`}
     </AutoLinkHeader>
-    <p {...{"className":"ws-p"}}>
+    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
       {`The search bar provides highlighted search results for better findability within a log viewer’s content.`}
     </p>
-    <img src={srcImport4} width={srcImport4.width} height={srcImport4.height} {...{"alt":"Log Viewer with highlighted search results","className":"ws-img"}}>
+    <img src={srcImport4} width={srcImport4.width} height={srcImport4.height} {...{"alt":"Log Viewer with highlighted search results","className":"ws-img "}}>
     </img>
   </React.Fragment>
 );
