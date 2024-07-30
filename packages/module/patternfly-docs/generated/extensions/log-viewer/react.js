acf54a287ce14f4741f4a50eb1c92ce472d1235c
Donald Labaj <donald.labaj@gmail.com>
2024-07-30 18:24:00 -0400

    feat(beta): Updated to latest beta version and merged main into v6.

--- packages/module/patternfly-docs/generated/extensions/log-viewer/react.js
+++ packages/module/patternfly-docs/generated/extensions/log-viewer/react.js
@@ -186,7 +186,7 @@ pageData.examples = {
 
 const Component = () => (
   <React.Fragment>
-    <AutoLinkHeader {...{"id":"examples","size":"h2","className":"ws-title ws-h2"}}>
+    <AutoLinkHeader {...{"id":"examples","headingLevel":"h2","className":"ws-title ws-h2"}}>
       {`Examples`}
     </AutoLinkHeader>
     {React.createElement(pageData.examples["Basic"])}
