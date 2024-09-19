0cdbdf6579c77ca6f3d51dabd27b3886086e9b1c
Donald Labaj <dlabaj@gmail.com>
2024-09-19 06:17:40 -0400

    chore: Updated to archieve main.

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -22,5 +22,6 @@ module.exports = {
     '@semantic-release/github',
     '@semantic-release/npm'
   ],
-  tagFormat: 'prerelease-v${version}'
+  tagFormat: 'prerelease-v${version}',
+  dryRun: true
 };
